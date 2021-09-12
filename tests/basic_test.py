de4b8ef1ced03aaf694a8c7e593f2f5e4d3e9859
Hari Prasad <h4ri.prasad@gmail.com>
2021-09-12 15:59:08 +0530

    Update basic_test.py

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -18,3 +18,21 @@ def test_vgg16():
                                  layer='features[30]')
 
     assert image_features[0].shape == (1, 512, 7, 7)
+
+def test_resnet18():
+    # Extract features using Feacher
+    image_features = feacher.extract(path,
+                                 pretrained_model='resnet18',
+                                 resize_dim=256,
+                                 layer=None)
+
+    assert image_features[0].shape == (1, 512, 1, 1)
+
+def test_alexnet():
+    # Extract features using Feacher
+    image_features = feacher.extract(path,
+                                 pretrained_model='alexnet',
+                                 resize_dim=256,
+                                 layer='features[12]')
+
+    assert image_features[0].shape == (1, 256, 6, 6)
